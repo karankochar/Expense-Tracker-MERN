042380ea3837031357380ae63d8a7308edca9d09
Karandeep <karankochar302gmail.com>
2020-06-04 23:11:25 +0530

    aa

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ if(process.env.NODE_ENV === 'production') {
 }
 
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
+app.listen(PORT, ()=>console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
 
 
