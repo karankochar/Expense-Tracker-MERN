d5084234244d0bbe003b5bc89e1fe1c1f3756021
Karandeep <karankochar302gmail.com>
2020-06-04 23:08:01 +0530

    aa

--- server.js
+++ server.js
@@ -25,7 +25,6 @@ if(process.env.NODE_ENV === 'production') {
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
 
-app.get('/', (req,res) => res.send('Hello'));
 
 const PORT = process.env.port || 5000;
 
