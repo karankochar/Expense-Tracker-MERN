882d9666ab04552ba4b20a27bf252002822dc52a
Karandeep <karankochar302gmail.com>
2020-06-05 00:28:48 +0530

    aa

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ if(process.env.NODE_ENV === 'production') {
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
 
-
+//abc
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, ()=>console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
